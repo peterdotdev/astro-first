6bd766f562df25573a18cb3a608579df52ca3ce8
peterdotdev <peterdotdev@gmail.com>
2024-04-10 11:15:03 -0300

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://peterdotdev.github.io',
   integrations: [tailwind(), preact()],
   output: 'hybrid' // server, hybrid o static (nada)
 });
