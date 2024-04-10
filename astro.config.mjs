b3cccbe3217b14a9507085df2fb4dde6f618597f
peterdotdev <peterdotdev@gmail.com>
2024-04-10 11:47:26 -0300

    couldnt handle server without adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
+// Cannot use `output: 'server'` or `output: 'hybrid'` without an adapter. 
+// Please install and configure the appropriate server adapter for your final deployment.
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://peterdotdev.github.io',
   integrations: [tailwind(), preact()],
-  output: 'hybrid' // server, hybrid o static (nada)
+  // output: 'hybrid' // server, hybrid o static (nada)
 });
